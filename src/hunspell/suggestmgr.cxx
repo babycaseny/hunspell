5776f6b87d09c3e45edb58a3186fe482244bb6db
groby@chromium.org <groby@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2013-02-27 01:43:24 +0000

    Use up to 8 affixes in Hunspell spellcheck suggestions
    Chrome has been suggesting "Othello" for "hellllo" and "identically" for
    "accidently" because it has been ignoring dictionary entries with more than one
    affix. This CL changes Chrome behavior to take these entries into consideration
    and use up to 8 affixes in suggestions.
    
    BUG=170668
    
    Review URL: https://codereview.chromium.org/12328121
    
    git-svn-id: http://src.chromium.org/svn/trunk/deps/third_party/hunspell@184822 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- src/hunspell/suggestmgr.cxx
+++ src/hunspell/suggestmgr.cxx
@@ -54,24 +54,25 @@ class ScopedHashEntryFactory {
   // it can store affix flags into 'h->astr[0]',...,'h->astr[alen-1]'. To handle
   // this new hentry struct, we define a struct which combines three values: an
   // hentry struct 'hentry'; a char array 'word[kMaxWordLen]', and; an unsigned
-  // short value 'astr' so a hentry struct 'h' returned from
+  // short array 'astr' so a hentry struct 'h' returned from
   // CreateScopedHashEntry() satisfies the following equations:
   //   hentry* h = factory.CreateScopedHashEntry(0, source);
   //   h->word[0] == ((HashEntryItem*)h)->entry.word[0].
   //   h->word[1] == ((HashEntryItem*)h)->word[0].
   //   ...
   //   h->word[h->blen] == ((HashEntryItem*)h)->word[h->blen-1].
-  //   h->astr[0] == ((HashEntryItem*)h)->astr.
-  // Our BDICT does not use affix flags longer than one for now since they are
-  // discarded by convert_dict, i.e. 'h->astr' is always <= 1. Therefore, this
-  // struct does not use an array for 'astr'.
+  //   h->astr[0] == ((HashEntryItem*)h)->astr[0].
+  //   h->astr[1] == ((HashEntryItem*)h)->astr[1].
+  //   ...
+  //   h->astr[h->alen-1] == ((HashEntryItem*)h)->astr[h->alen-1].
   enum {
     kMaxWordLen = 128,
+    kMaxAffixLen = 8,
   };
   struct HashEntryItem {
     hentry entry;
     char word[kMaxWordLen];
-    unsigned short astr;
+    unsigned short astr[kMaxAffixLen];
   };
 
   HashEntryItem hash_items_[MAX_ROOTS];
@@ -86,7 +87,7 @@ ScopedHashEntryFactory::~ScopedHashEntryFactory() {
 
 hentry* ScopedHashEntryFactory::CreateScopedHashEntry(int index,
                                                       const hentry* source) {
-  if (index >= MAX_ROOTS || source->blen >= kMaxWordLen || source->alen > 1)
+  if (index >= MAX_ROOTS || source->blen >= kMaxWordLen)
     return NULL;
 
   // Retrieve a HashEntryItem struct from our spool, initialize it, and
@@ -95,8 +96,11 @@ hentry* ScopedHashEntryFactory::CreateScopedHashEntry(int index,
   HashEntryItem* hash_item = &hash_items_[index];
   memcpy(&hash_item->entry, source, source_size);
   if (source->astr) {
-    hash_item->astr = *source->astr;
-    hash_item->entry.astr = &hash_item->astr;
+    hash_item->entry.alen = source->alen;
+    if (hash_item->entry.alen > kMaxAffixLen)
+      hash_item->entry.alen = kMaxAffixLen;
+    memcpy(hash_item->astr, source->astr, hash_item->entry.alen * sizeof(hash_item->astr[0]));
+    hash_item->entry.astr = &hash_item->astr[0];
   }
   return &hash_item->entry;
 }
